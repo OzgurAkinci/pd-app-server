d470940b5cb717d900397c57954c1481b964bd9c
Özgür Akıncı <akincior@gmail.com>
2020-11-11 20:49:48 +0300

    first commit

---
+++ app/Controllers/User.php
@@ -0,0 +1,21 @@
+<?php namespace App\Controllers;
+
+class User extends RestController
+{
+    
+    function __construct()
+    {
+        // Construct the parent class
+        parent::__construct();
+    }
+
+	 
+	public function index()
+	{
+		
+		return "app start..";
+	}
+
+	//--------------------------------------------------------------------
+
+}
